b92bde908c5eb44330740ca014869520da9be401
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2018-03-14 14:08:49 +0100

    fix(wdio): cleanup

--- test/wdio/wdio.conf.js
+++ test/wdio/wdio.conf.js
@@ -21,11 +21,11 @@ exports.config = {
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
   specs: [
-    './tests/*.js'
+    './test/*.js'
   ],
   // Patterns to exclude.
   exclude: [
-    // 'path/to/excluded/files'
+    './test/wdio/*.js'
   ],
   //
   // ============
