1cec6431bce120800b1f70ed5b89887a22dc154f
Stefan Eckert <MrTelanie@gmail.com>
2018-10-20 13:49:48 +0200

    feat(Container): use ohta and lab false color

---
+++ src/workers/image/ohta-false-color.worker.js
@@ -0,0 +1,32 @@
+import '../image.base';
+import { rgb2ohta } from '../../utils/rgb-ohta';
+
+self.options = { channels: 1 };
+self.start = function() {};
+
+let maxRed = Number.NEGATIVE_INFINITY;
+let maxBlue = Number.NEGATIVE_INFINITY;
+
+self.tick = function(px) {
+  const otha = rgb2ohta(px);
+
+  const value = otha[self.options.channels];
+
+  if (value > 0) {
+    maxRed = Math.max(maxRed, Math.abs(value));
+    const color = (value / maxRed) * 255;
+
+    px[0] = Math.round(color);
+    px[1] = 0;
+    px[2] = 0;
+  } else {
+    maxBlue = Math.max(maxBlue, Math.abs(value));
+    const color = (value / maxBlue) * 255;
+
+    px[0] = 0;
+    px[1] = 0;
+    px[2] = Math.round(color);
+  }
+};
+
+self.end = function() {};
