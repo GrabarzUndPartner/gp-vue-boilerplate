db5cbad9a8827e2b0ee2083afc19f466da379bb0
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2018-10-23 20:37:21 +0200

    fix(repeat): one more time

--- env/webpack/modules/vue-loader.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(rules) {
-  rules.push({
-    resourceQuery: /\bblockType=i18n\b/,
-    use: [{ loader: '@kazupon/vue-i18n-loader' }],
-    type: 'javascript/auto'
-  });
-  return [];
-};
