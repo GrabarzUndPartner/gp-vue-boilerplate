3e40a8aad9071e3e805b233ee43b3ed64ac80b0d
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2019-06-15 17:59:23 +0200

    fix(license): added missed license

--- env/nuxt.config.js
+++ env/nuxt.config.js
@@ -261,7 +261,8 @@ module.exports = {
             LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
             OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
             SOFTWARE.`,
-          'consola': 'MIT License'
+          'consola': 'MIT License',
+          'intersection-observer': 'W3C Software and Document License'
         }
       }
     ]
