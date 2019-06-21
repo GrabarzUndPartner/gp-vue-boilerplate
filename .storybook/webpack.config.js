27b80b2b51274dbd4ed8b3acc0eb06b8e8094da6
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2019-06-21 14:28:36 +0200

    fix(storybook): added lazy hydrate + svg support

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -24,5 +24,36 @@ module.exports = ({ config }) => {
     ]
   });
 
+  config.module.rules = config.module.rules.map(rule => {
+    if (rule.test && rule.test.toString().includes('svg')) {
+      const test = rule.test.toString().replace('svg|', '').replace(/\//g, '')
+      return { ...rule, test: new RegExp(test) }
+    } else {
+      return rule
+    }
+  });
+
+  config.module.rules.push({
+    test: /\.svg$/,
+    oneOf: [{
+      resourceQuery: /include/,
+      use: [{
+        loader: 'raw-loader',
+        options: {}
+      }, {
+        loader: 'svgo-loader',
+        options: {
+          externalConfig: '.svgorc.yml'
+        }
+      }]
+    },
+    {
+      use: [{
+        loader: 'file-loader',
+        options: {}
+      }]
+    }]
+  });
+
   return config;
 }
