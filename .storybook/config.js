77d4590ff547709bd868e6fb3ca87f90ffb49c23
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2019-06-24 08:44:07 +0200

    fix(travis): deploy multiple builds to gh-pages

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,8 @@
 import { configure } from '@storybook/vue';
 import '@/plugins/lazyHydrate';
 
-
 // automatically import all files ending in *.stories.js
-const req = require.context('../src/components', true, /\.stories\.js$/);
+const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories () {
   req.keys().forEach(filename => req(filename));
 }
