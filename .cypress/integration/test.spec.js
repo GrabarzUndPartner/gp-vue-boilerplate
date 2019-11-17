ced1d4ca49554ae896ff63c0ac48091f24084ff5
Thorn Walli <thorn.walli@grabarzundpartner.de>
2019-11-17 14:58:52 +0100

    fix(cypress-test): fix test "test"

--- .cypress/integration/test.spec.js
+++ .cypress/integration/test.spec.js
@@ -3,9 +3,15 @@ import { createTestInAllResolutions } from '../utils/viewport';
 createTestInAllResolutions((res) => {
   describe(`homepage`, function () {
 
-    it('test the language switch', function () {
+
+
+    it('test the menu button and language switch', function () {
       cy.visit('/');
-      cy.get('.language-switch').click();
+      cy.get('.gp-page-menu-button').first().click();
+      cy.get('.language-switch').first().click({ timeout: 1000 });
+
     });
+
+
   });
 });
