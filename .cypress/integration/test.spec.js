e7822fda107a3cd686a73f10c792f7822c0dcb2d
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2019-11-17 17:21:08 +0100

    fix(cypress): delayed click on language switch

--- .cypress/integration/test.spec.js
+++ .cypress/integration/test.spec.js
@@ -3,15 +3,16 @@ import { createTestInAllResolutions } from '../utils/viewport';
 createTestInAllResolutions((res) => {
   describe(`homepage`, function () {
 
-
-
-    it('test the menu button and language switch', function () {
+    it('menu button', function () {
       cy.visit('/');
       cy.get('.gp-page-menu-button').first().click();
-      cy.get('.language-switch').first().click({ timeout: 1000 });
-
     });
 
+    it('language switch', function () {
+      cy.get('.gp-organism-menu').should('have.class', 'js--visible').then(() => {
+        cy.get('.language-switch').first().click();
+      })
+    });
 
   });
 });
