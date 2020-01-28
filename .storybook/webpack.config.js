b81e59c05ba96178b8c876e7e49b3fc224c3f15a
Thorn Walli <thorn.walli@grabarzundpartner.de>
2020-01-28 16:18:02 +0100

    fix(storybook): fix postcss plugins

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,22 @@
 const { Nuxt } = require('nuxt');
 const nuxt = new Nuxt(require('../nuxt.config.js'))
 const svg = require('../src/modules/svg');
+const postcssPresetEnv = require('postcss-preset-env');
 
 const postcssOptions = Object.assign({}, nuxt.options.build.postcss);
 postcssOptions.plugins = Object.keys(postcssOptions.plugins).map(pluginName => {
   return require(pluginName)(postcssOptions.plugins[pluginName]);
 });
 
+postcssOptions.plugins.push(postcssPresetEnv(postcssOptions.preset))
+delete postcssOptions.preset;
+
 module.exports = ({ config }) => {
   config.resolve.alias = Object.assign(config.resolve.alias, nuxt.options.alias);
   svg.bind({ extendBuild (fn) { fn(config) } })();
 
   config.module.rules.push({
-    resourceQuery: /postcss/,
+    test: /\.p(ost)?css$/i,
     use: [
       'style-loader',
       'css-loader',
