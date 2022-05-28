f589989d30c2ecd50ee0caa75cef450b36e0d79c
Thorn Walli <lammpee@gmail.com>
2022-05-28 18:06:08 +0200

    fix(postcss): update `lost` & `postcss-preset-env`

--- src/globals/postcss/preset-env/importFrom.js
+++ src/globals/postcss/preset-env/importFrom.js
@@ -1,5 +1,12 @@
 
-export { default as customMedia } from './custom-media.json';
-export { default as customSelectors } from './custom-selector.json';
-export { default as customProperties } from './custom-properties.json';
-export { default as environmentVariables } from './environment-variables.json';
+import customMedia from './custom-media.json';
+import customSelectors from './custom-selector.json';
+import customProperties from './custom-properties.json';
+import environmentVariables from './environment-variables.json';
+
+export default {
+  customMedia,
+  customSelectors,
+  customProperties,
+  environmentVariables
+};
